d00eb947f0079f6835af00bccd02f3617dc1cb04
Jackson Cafazzo <doctorjuno@gmail.com>
2016-01-19 21:18:44 -0800

    Refactor destinations into an array and display at end based on index

--- js/scripts.js
+++ js/scripts.js
@@ -6,56 +6,36 @@ $(document).ready(function() {
     var hemi = $("select#hemi").val();
     var devel = $("select#devel").val();
     var cheese = $("select#cheese").val();
+    var destResults = ['#Milwaukee', '#PortlandMaine', '#elPaso', '#Brazil', '#Antarctica'];
+    $(destResults.join(', ')).fadeOut();
 
       if (river === "riverYes" && temp === "tempCool" && hemi === "hemiNorth" && devel === "develYes" && cheese === "cheeseYes") {
         var result = "yes";
-        $('#Milwaukee').fadeIn();
-        $('#PortlandMaine').hide();
-        $('#elPaso').hide();
-        $('#Brazil').hide();
-        $('#Antarctica').hide();
-        $('#whoops').hide();
+        var index = 0
       }
 
       if (river === "riverYes" && temp === "tempCool" && hemi === "hemiNorth" && devel === "develYes" && cheese === "cheeseNo") {
         var result = "yes";
-        $('#PortlandMaine').fadeIn();
-        $('#elPaso').hide();
-        $('#Brazil').hide();
-        $('#Milwaukee').hide();
-        $('#Antarctica').hide();
-        $('#whoops').hide();
+        var index = 1
       }
 
       if (river === "riverYes" && temp === "tempHot" && hemi === "hemiNorth" && devel === "develYes" && cheese === "cheeseYes") {
         var result = "yes";
-        $('#Antarctica').hide();
-        $('#elPaso').fadeIn();
-        $('#Brazil').hide();
-        $('#Milwaukee').hide();
-        $('#PortlandMaine').hide();
-        $('#whoops').hide();
+        var index = 2
       }
 
       if (river === "riverYes" && temp === "tempHot" && hemi === "hemiSouth" && devel === "develYes" && cheese === "cheeseYes") {
         var result = "yes";
-        $('#Antarctica').hide();
-        $('#Brazil').fadeIn();
-        $('#elPaso').hide();
-        $('#Milwaukee').hide();
-        $('#PortlandMaine').hide();
-        $('#whoops').hide();
+        var index = 3
       }
 
       if (river === "riverNo" && temp === "tempCool" && hemi === "hemiSouth" && devel === "develNo" && cheese === "cheeseNo") {
         var result = "yes";
-        $('#Antarctica').fadeIn();
-        $('#Brazil').hide();
-        $('#elPaso').hide();
-        $('#Milwaukee').hide();
-        $('#PortlandMaine').hide();
-        $('#whoops').hide();
+        var index = 4
       }
+
+      $(destResults[index]).fadeIn();
+
 
       if (result === "no") {
         alert('Whoops, no results for those selections, try something else!')
